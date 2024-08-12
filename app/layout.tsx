54bac3e90ede07a256fa3e1b54a976b1abcdc47e
Arjun Dabir <arjunadabir@gmail.com>
2024-08-11 18:03:42 -0700

    chore: fixed gtag, added button for projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,27 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="">
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KGLK9N5V');
-          `,
-          }}
-        />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-XW1J9QKM39"></Script>
+        <Script id="google analytics">
+          {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-XW1J9QKM39'); `}
+        </Script>
       </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGLK9N5V"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
-          `,
-          }}
-        />
         <main className="flex-1 h-full">{children}</main>
         <Footer />
       </body>
